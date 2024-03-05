c3657510dbf33bf322d5a3966c190e3b4fa52b2b
talisson <talissonb422@gmail.com>
2024-03-05 01:55:03 -0300

    fix responsive to mobile

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const buttonClose = document.querySelector('#btn-close')
 const buttonWatch = document.querySelector('#btn-watch')
 const modal = document.querySelector('#modal')
+const btnMenu = document.querySelector('.btn-menu')
+const btnMenuClose = document.querySelector('.btn-menu-close')
+const menu = document.querySelector('.menu')
 
 buttonClose.addEventListener('click', () => {
   modal.classList.remove('is-visibily')
@@ -8,4 +11,12 @@ buttonClose.addEventListener('click', () => {
 
 buttonWatch.addEventListener('click', () => {
   modal.classList.add('is-visibily')
+})
+
+btnMenu.addEventListener('click', () => {
+  menu.classList.add('is-open')
+})
+
+btnMenuClose.addEventListener('click' ,() => {
+  menu.classList.remove('is-open')
 })
